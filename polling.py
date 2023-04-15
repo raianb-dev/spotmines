99bb402b99d9155ce863b8bf7f6c560449577616
RAIAN BARBOSA <raianpbstudio@gmail.com>
2023-04-15 16:23:23 -0300

    ajustes no polling

--- polling.py
+++ polling.py
@@ -1,30 +1,6 @@
-import os
 import subprocess
 import time
 
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-
-class GitPullHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if event.src_path.endswith('.git/index.lock'):
-            # Ignore git lock files
-            return
-        print(f"Change detected in {event.src_path}, pulling changes")
-        subprocess.call(['git', 'pull'])
-
-
-if __name__ == '__main__':
-    event_handler = GitPullHandler()
-    observer = Observer()
-    observer.schedule(event_handler, '.', recursive=True)
-    observer.start()
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-
-    observer.join()
+while True:
+    subprocess.run(['git', 'pull'])
+    time.sleep(300) 
