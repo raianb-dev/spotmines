37e6ad2e570de87d9ea56b175f096e1aa49fd5ee
RAIAN BARBOSA <raianpbstudio@gmail.com>
2023-04-15 15:53:29 -0300

    add. config debug

--- serverSettings/urls.py
+++ serverSettings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from Raioxmines import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('accounts/login/', views.user_login, name='user_login'),
@@ -8,3 +9,6 @@ urlpatterns = [
   path('accounts/', views.welcome, name='welcome'),
   
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
