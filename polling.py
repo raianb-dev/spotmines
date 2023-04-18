f2f501fbff5c63dac237e47a72779f899b2d67d8
RAIAN BARBOSA <raianpbstudio@gmail.com>
2023-04-18 14:01:24 -0300

    changes polling

--- polling.py
+++ polling.py
@@ -5,4 +5,5 @@ import time
 while True:
     subprocess.run(['git', 'pull'])
     os.system('find /tmp -type f -iname "gunicorn*" -delete')
+    subprocess.run(['python3', 'manage.py', 'collectstatic', '--noinput'])
     time.sleep(60)
