eaadeebad0477d5c30d331f2fa3a43fbc153735f
RAIAN BARBOSA <raianpbstudio@gmail.com>
2023-05-07 23:46:26 -0300

    bug denovo

--- _serverSettings/settings.py
+++ _serverSettings/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = ['/static/']
-
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
