3dad78aca9646ffb81c55aca04207dbae453bdea
RAIAN BARBOSA <raianpbstudio@gmail.com>
2023-05-11 15:59:36 -0300

    adicionando aviatrix

--- _serverSettings/urls.py
+++ _serverSettings/urls.py
@@ -12,22 +12,23 @@ from pressell import views as views_pressell
 from aviatrix import views as views_aviatrix
 from notfound import views as views_404
 
-
 urlpatterns = [
   # URLS DEFAULT
   path('', views.user_login, name='welcome'),
-  path('vsl/', views_vsl.vsl, name='vsl'),
+  path('vsl', views_vsl.vsl, name='vsl'),
   path('logout', LogoutView.as_view(next_page=reverse_lazy('user_login')), name='logout'),
-  path('login/', views.user_login, name='user_login'),
-  path('mines/', views_mines.mines, name='mines'),
-  path('nostar/', views.nostar, name='nostar'),
-  path('star/', views.star, name='star'),
+  path('login', views.user_login, name='user_login'),
+  path('mines', views_mines.mines, name='mines'),
+  path('nostar', views.nostar, name='nostar'),
+  path('star', views.star, name='star'),
   path('webhooks', views.webhook, name='webhook'),
-  path('double/', views_double.double_view, name="double" ),
+  path('double', views_double.double_view, name="double" ),
   path("pressell/", views_pressell.pressell, name="pressell"),
   path("pressell-pixel/", views_pressell.pressell_pixel, name="pressell-pixel"),
-  path('homepage/', views_homepage.homepage, name='homepage'),
-  path('aviatrix/', views_aviatrix.aviatrix, name='aviatrix'),
+  path('homepage', views_homepage.homepage, name='homepage'),
+  path('aviatrix', views_aviatrix.aviatrix, name='aviatrix'),
+  path('json-results', views_aviatrix.json_results, name='json-results'),
+  path('get_last_results',  views_aviatrix.get_last_results, name='get_last_results'),
   path('<path:undefined_path>', views.notfound, name='404'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- aviatrix/views.py
+++ aviatrix/views.py
@@ -1,6 +1,51 @@
+import requests
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 @login_required
 def aviatrix(request):
     return render(request, 'aviatrix.html')
+
+def get_last_results(request):
+    try:
+        response = requests.post("https://aviatrix-gateway-nft-prod.aviatrix.bet/v2/aviatrix.gateway.Api/GetGameHistory", json={"limit": 5})
+        response.raise_for_status()  # Verifica se houve algum erro na resposta HTTP
+        game_history = response.json()
+
+        context = {
+            'gameHistory': game_history['games']
+        }
+
+        return render(request, 'last_results.html', context)
+    except requests.exceptions.RequestException as e:
+        # Tratamento de erro
+        error_message = "Ocorreu um erro na requisição: " + str(e)
+        return JsonResponse({'error': error_message}, status=500)
+
+import requests
+from django.http import JsonResponse
+
+import requests
+from django.http import JsonResponse
+
+def json_results(request):
+    try:
+        response = requests.post("https://aviatrix-gateway-nft-prod.aviatrix.bet/v2/aviatrix.gateway.Api/GetGameHistory", json={"limit": 1})
+        response.raise_for_status()  # Verifica se houve algum erro na resposta HTTP
+        game_history = response.json()
+
+        # Verifica se há jogos na resposta
+        if 'games' in game_history:
+            games = game_history['games']
+
+            # Verifica cada jogo individualmente
+            for game in games:
+                if 'random' in game and game['random'] > 1:
+                    game['cor'] = '2x'
+
+        return JsonResponse(game_history)
+    except requests.exceptions.RequestException as e:
+        # Tratamento de erro
+        error_message = "Ocorreu um erro na requisição: " + str(e)
+        return JsonResponse({'error': error_message}, status=500)
