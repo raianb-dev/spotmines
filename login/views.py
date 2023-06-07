b0e7baa3143553df637d46719efc4360e657e680
RAIAN BARBOSA <raianpbstudio@gmail.com>
2023-06-07 14:31:23 -0300

    remove buttons

--- login/views.py
+++ login/views.py
@@ -58,7 +58,7 @@ from django.contrib.auth import authenticate, login
 def user_login(request):
     if request.method == 'POST':
         email = request.POST.get('email')
-        password = 'acesso.mines'
+        password = 'admin'
         q_email = Users.objects.filter(email=email)
   
         if q_email.exists():
